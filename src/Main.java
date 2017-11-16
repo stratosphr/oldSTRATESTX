f384c29dc135c665e2c4ea7e5f8632fb10efdb4b
stratosphr <stratosphr@gmail.com>
2017-11-16 18:39:44 +0100

    XML parser generating tree with nodes having file, lines and column information

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
+        /*XMLParsedNode root = new XMLParser(true).parse(new File("path/to/xml/file.some.extension"));
+        System.out.println(root);*/
     }
 
 }
